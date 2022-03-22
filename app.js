b887daea970b72082e872d06c51d1878397791b2
Alejandro Campos <acamposcar@gmail.com>
2022-03-22 14:26:10 +0100

    Create grid

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+/* eslint-disable no-plusplus */
 // @ts-check
+
+const size = 16;
+const container = document.querySelector('#sketch');
+container.style.cssText = `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr)`;
+
+for (let i = 0; i < size * size; i++) {
+  const content = document.createElement('div');
+  content.classList.add('item');
+  container.appendChild(content);
+}
