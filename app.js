7d36dddbede297fdf52bfd8c3f5f59eaae648c3e
Alejandro Campos <acamposcar@gmail.com>
2022-03-22 21:41:04 +0100

    Add JS and made initial styling

--- app.js
+++ app.js
@@ -1,12 +1,104 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-plusplus */
-// @ts-check
+// ts-check
 
-const size = 16;
-const container = document.querySelector('#sketch');
-container.style.cssText = `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr)`;
+// Global variable
+let mode = 'color';
 
-for (let i = 0; i < size * size; i++) {
-  const content = document.createElement('div');
-  content.classList.add('item');
-  container.appendChild(content);
+function colorMode() {
+  const color = document.querySelector('#color-mode').value;
+  return color;
 }
+
+function rainbowMode() {
+  const hue = Math.floor(Math.random() * 360);
+  const color = `hsl(${hue}, 100%, 50%)`;
+  return color;
+}
+
+function erase() {
+  return 'white';
+}
+
+function clear() {
+  document.querySelectorAll('.item').forEach((item) => {
+    item.style.backgroundColor = 'white';
+  });
+}
+
+function selectMode() {
+  switch (mode) {
+    case 'color':
+      return colorMode();
+    case 'rainbow':
+      return rainbowMode();
+    case 'erase':
+      return erase();
+    default:
+      return colorMode();
+  }
+}
+
+function buttonListeners() {
+  const colorButton = document.querySelector('#color-mode');
+  const rainbowButton = document.querySelector('#rainbow-mode');
+  const eraseButton = document.querySelector('#erase-mode');
+  const clearButton = document.querySelector('#clear');
+  const gridSlider = document.querySelector('#grid-slider');
+
+  colorButton.addEventListener('click', () => {
+    mode = 'color';
+    colorButton.classList.add('active');
+    rainbowButton.classList.remove('active');
+    eraseButton.classList.remove('active');
+  });
+
+  rainbowButton.addEventListener('click', () => {
+    mode = 'rainbow';
+    colorButton.classList.remove('active');
+    rainbowButton.classList.add('active');
+    eraseButton.classList.remove('active');
+  });
+
+  eraseButton.addEventListener('click', () => {
+    mode = 'erase';
+    colorButton.classList.remove('active');
+    rainbowButton.classList.remove('active');
+    eraseButton.classList.add('active');
+  });
+
+  clearButton.addEventListener('click', () => {
+    clear();
+  });
+
+  gridSlider.addEventListener('input', () => createGrid());
+}
+
+function getGridSize() {
+  const gridOptions = [10, 16, 32, 64, 96];
+  const gridSliderValue = document.querySelector('#grid-slider').value;
+  const gridSize = gridOptions[gridSliderValue];
+  document.querySelector('#grid-size').textContent = `${gridSize} x ${gridSize}`;
+  return gridSize;
+}
+
+function createGrid() {
+  const size = getGridSize();
+  const sketch = document.querySelector('#sketch');
+  sketch.innerHTML = '';
+  sketch.style.cssText = `grid-template-columns: repeat(${size}, 1fr); `;
+
+  clear();
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('item');
+    sketch.appendChild(square);
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = selectMode();
+    });
+  }
+}
+
+// Initiate all
+buttonListeners();
+createGrid();
