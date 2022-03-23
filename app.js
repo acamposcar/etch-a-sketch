698839836cb63b855d4a46711a55963ae349a452
Alejandro Campos <acamposcar@gmail.com>
2022-03-23 01:14:40 +0100

    Make responsive and add dark mode

--- app.js
+++ app.js
@@ -4,25 +4,46 @@
 
 // Global variable
 let mode = 'color';
+// const sketchColor = 'rgb(226, 226, 226)';
+let sketchColor = '#f1f1f1';
+let darkEnabled = false;
+
+function darkMode() {
+  darkEnabled = !darkEnabled;
+  sketchColor = darkEnabled ? '#141616' : '#f1f1f1';
+  document.querySelector('body').classList.toggle('dark');
+  document.querySelector('#sketch').classList.toggle('dark');
+  document.querySelector('#moon').classList.toggle('dark');
+  document.querySelector('.item').classList.toggle('dark');
+  document.querySelector('input[type="color"]').classList.toggle('dark');
+  document.querySelectorAll('button').forEach((button) => {
+    button.classList.toggle('dark');
+  });
+
+  clear();
+}
 
 function colorMode() {
-  const color = document.querySelector('#color-mode').value;
+  /* Users selects color in the color picker */
+  const color = document.querySelector('#color-picker').value;
   return color;
 }
 
 function rainbowMode() {
+  /* Generates a random color in HSL */
   const hue = Math.floor(Math.random() * 360);
   const color = `hsl(${hue}, 100%, 50%)`;
   return color;
 }
 
 function erase() {
-  return 'white';
+  /* Same color as background */
+  return sketchColor;
 }
 
 function clear() {
   document.querySelectorAll('.item').forEach((item) => {
-    item.style.backgroundColor = 'white';
+    item.style.backgroundColor = sketchColor;
   });
 }
 
@@ -45,12 +66,16 @@ function buttonListeners() {
   const eraseButton = document.querySelector('#erase-mode');
   const clearButton = document.querySelector('#clear');
   const gridSlider = document.querySelector('#grid-slider');
-
+  const colorPicker = document.querySelector('#color-picker');
+  const darkButton = document.querySelector('#dark');
+  // Add active class in selected mode
   colorButton.addEventListener('click', () => {
     mode = 'color';
     colorButton.classList.add('active');
     rainbowButton.classList.remove('active');
     eraseButton.classList.remove('active');
+    // Simulate click in the input color picker
+    colorPicker.click();
   });
 
   rainbowButton.addEventListener('click', () => {
@@ -67,11 +92,9 @@ function buttonListeners() {
     eraseButton.classList.add('active');
   });
 
-  clearButton.addEventListener('click', () => {
-    clear();
-  });
-
-  gridSlider.addEventListener('input', () => createGrid());
+  clearButton.addEventListener('click', clear);
+  gridSlider.addEventListener('input', createGrid);
+  darkButton.addEventListener('click', darkMode);
 }
 
 function getGridSize() {
@@ -88,7 +111,6 @@ function createGrid() {
   sketch.innerHTML = '';
   sketch.style.cssText = `grid-template-columns: repeat(${size}, 1fr); `;
 
-  clear();
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
     square.classList.add('item');
@@ -96,7 +118,11 @@ function createGrid() {
     square.addEventListener('mouseover', () => {
       square.style.backgroundColor = selectMode();
     });
+    square.addEventListener('mousedown', () => {
+      square.style.backgroundColor = selectMode();
+    });
   }
+  clear();
 }
 
 // Initiate all
